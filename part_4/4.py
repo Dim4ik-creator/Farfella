0731f24fd5291593356ae7277d1ea8f7dbdec130
Dim4ik-creator <141348723+Dim4ik-creator@users.noreply.github.com>
2025-02-26 12:42:37 +0300

    Описание изменений

--- part_4/4.py
+++ part_4/4.py
@@ -4,4 +4,8 @@ n = int(input("Введите длину списка: "))
 l = []
 for i in range(n):
     l.append(int(input("Введите элемент списка: ")))
-min(l), max(l)
+print("Исходный список:\n", l)
+min_index = l.index(min(l))
+max_index = l.index(max(l))
+l[min_index], l[max_index] = l[max_index], l[min_index]
+print("Изменённый список:\n",l)

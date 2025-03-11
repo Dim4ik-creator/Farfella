b7a885e54d6ecfe0903f2cc9e18a1e324a322b9c
Dim4ik-creator <141348723+Dim4ik-creator@users.noreply.github.com>
2025-03-11 13:21:32 +0300

    Описание изменений

--- part_6/3.py
+++ part_6/3.py
@@ -1,1 +1,23 @@
 """3. Разделить элементы списка на минимальный элемент."""
+
+
+def min_list(l):
+    mn = float("inf")
+    for i in range(len(l)):
+        if l[i] < mn:
+            mn = l[i]
+    return mn
+
+
+def devide_min(l):
+    mn = min_list(l)
+    if mn == 0:
+        return "Минимальный элемент равен 0, деление невозможно"
+    for i in range(len(l)):
+        l[i] = l[i] / mn
+    return l
+
+
+n = int(input("Введите размер списка: "))
+l = [int(input("Введите элемент списка: ")) for i in range(n)]
+print(devide_min(l))
--- part_6/5.py
+++ part_6/5.py
@@ -1,1 +1,18 @@
 """5. В списке определить количество четных и нечетных элементов."""
+
+
+def even_and_odd(l):
+    even = 0
+    odd = 0
+    for i in range(len(l)):
+        if l[i] % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    return even, odd
+
+
+n = int(input("Введите размер списка: "))
+l = [int(input("Введите элемент списка: ")) for i in range(n)]
+even, odd = even_and_odd(l)
+print(f"Количество четных элементов:{even}\nКоличество нечетных элементов:{odd}")
